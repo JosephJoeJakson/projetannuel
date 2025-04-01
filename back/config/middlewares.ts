7bc145ac7372935dfe3f0e8679296ff1f81c5a2d
Kaluzny Adrien <kaluzny@2a02-8429-80d0-fd01-4223-43ff-fe7e-9d99.rev.sfr.net>
2025-04-01 18:15:06 +0200

    add: fields

--- back/config/middlewares.ts
+++ back/config/middlewares.ts
@@ -1,8 +1,16 @@
 export default [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000'], // 👈 Your frontend URL
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  },
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
