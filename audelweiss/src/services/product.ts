81e735c11a4301c04c173d014bc1716d60c7d90d
Kadem <kadem.garnier@gmail.com>
2025-04-02 15:04:31 +0200

    with strapi api shortcut

--- audelweiss/src/services/product.ts
+++ audelweiss/src/services/product.ts
@@ -1,22 +1,13 @@
-import { API_URL } from '@/utils/config';
-import {Product} from "@/types/product";
+import { Product } from '@/types/product';
+import {getRequest} from "../../lib/strapi";
 
-export async function fetchProducts() {
-    const res = await fetch(`${API_URL}/api/products?populate=*`);
-
-    if (!res.ok) throw new Error('Erreur lors de la récupération des produits');
-    return (await res.json()).data;
+export async function fetchProducts(): Promise<Product[]> {
+    const data = await getRequest('products?populate=*');
+    return data?.data || [];
 }
 
 export async function fetchProductById(id: string): Promise<Product | null> {
-    const res = await fetch(`${API_URL}/api/products?filters[id][$eq]=${id}&populate=*`);
-    if (!res.ok) return null;
-
-    const json = await res.json();
-    const product = json.data[0];
-
-    if (!product) return null;
-
-    return product;
+    const data = await getRequest(`products?filters[id][$eq]=${id}&populate=*`);
+    const product = data?.data?.[0];
+    return product || null;
 }
-
