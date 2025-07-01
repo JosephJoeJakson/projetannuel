0c0eba21956630b349d6fd2712c0725bc23ea6ff
Kadem <kadem.garnier@gmail.com>
2025-07-01 14:21:15 +0200

    add footer automatic, and other css, fix login and register with authcontext

--- back/types/generated/components.d.ts
+++ back/types/generated/components.d.ts
@@ -1,5 +1,27 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface CommonFooterHelpMenu extends Struct.ComponentSchema {
+  collectionName: 'components_common_footer_help_menus';
+  info: {
+    description: "Menu Besoin d'aide du footer";
+    displayName: 'footer-help-menu';
+  };
+  attributes: {
+    link: Schema.Attribute.Component<'common.link', true>;
+  };
+}
+
+export interface CommonFooterMenu extends Struct.ComponentSchema {
+  collectionName: 'components_common_footer_menus';
+  info: {
+    description: 'Menu du footer';
+    displayName: 'footer-menu';
+  };
+  attributes: {
+    link: Schema.Attribute.Component<'common.link', true>;
+  };
+}
+
 export interface CommonLink extends Struct.ComponentSchema {
   collectionName: 'components_common_links';
   info: {
@@ -39,6 +61,19 @@ export interface CommonNavbar extends Struct.ComponentSchema {
   };
 }
 
+export interface CommonSocialLink extends Struct.ComponentSchema {
+  collectionName: 'components_common_social_links';
+  info: {
+    description: 'Lien de r\u00E9seau social';
+    displayName: 'social-link';
+  };
+  attributes: {
+    icon: Schema.Attribute.String;
+    name: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
 export interface ProductKeyValuePair extends Struct.ComponentSchema {
   collectionName: 'components_product_key_value_pairs';
   info: {
@@ -53,9 +88,12 @@ export interface ProductKeyValuePair extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'common.footer-help-menu': CommonFooterHelpMenu;
+      'common.footer-menu': CommonFooterMenu;
       'common.link': CommonLink;
       'common.mega-menu': CommonMegaMenu;
       'common.navbar': CommonNavbar;
+      'common.social-link': CommonSocialLink;
       'product.key-value-pair': ProductKeyValuePair;
     }
   }
